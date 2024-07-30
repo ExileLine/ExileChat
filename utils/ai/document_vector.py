54d8dc34dad402549b93095f0f7a52824b454894
yangyuexiong <yang6333yyx@126.com>
2024-07-31 02:01:32 +0800

    feat:`DocumentVector`文档向量

--- utils/ai/document_vector.py
+++ utils/ai/document_vector.py
@@ -5,6 +5,8 @@
 # @File    : document_vector.py
 # @Software: PyCharm
 
+import json
+
 from utils.ai.llm_engine import LLMEngine
 
 
@@ -22,8 +24,39 @@ class DocumentVector:
         self.llm_engine = llm_engine
         self.prompt = prompt
 
-    async def main(self):
-        """main"""
+    async def gen_qa_chunks(self):
+        """生成`QA chunks`"""
 
-        result_content = await self.llm_engine.chat_only(prompt=self.prompt, input=self.document_content)
-        return result_content
+        qa_chunks_str = await self.llm_engine.chat_only(prompt=self.prompt, input=self.document_content)
+        if qa_chunks_str:
+            print(type(qa_chunks_str), qa_chunks_str)
+            try:
+                qa_chunks_json = json.loads(qa_chunks_str)
+                return qa_chunks_json
+            except BaseException as e:
+                raise TypeError(f"qa_chunks JSON序列化失败：{e}")
+        else:
+            raise TypeError(f"qa_chunks JSON序列化失败2")
+
+    async def gen_vector_qa_chunks(self, chunks: list[dict]) -> list[dict]:
+        """生成`QA chunks`向量"""
+
+        vectored_qa_chunks = []
+
+        for chunk in chunks:
+            question = chunk.get("Q")
+            answer = chunk.get("A")
+            question_embedding = await self.llm_engine.embedding(text=question)
+            answer_embedding = await self.llm_engine.embedding(text=answer)
+
+            data = {
+                "able_id": 0,
+                "document_id": 0,
+                "answer": answer,
+                "question": question,
+                "question_embedding": json.dumps(question_embedding, ensure_ascii=False),
+                "answer_embedding": json.dumps(answer_embedding, ensure_ascii=False)
+            }
+            vectored_qa_chunks.append(data)
+
+        return vectored_qa_chunks
